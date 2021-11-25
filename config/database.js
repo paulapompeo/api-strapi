cdbad3342a88c06b0a3c34af96eff6599223fd06
paulapompeo <paulapompeoc@gmail.com>
2021-11-25 10:08:12 +0100

    adds ssl

--- config/database.js
+++ config/database.js
@@ -10,9 +10,13 @@ hmodule.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
-      options: {}
+      options: {
+        ssl: true,
+      }
     },
   },
 });
